eb09ba4535577dd9e372157941daed28be27db36
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-04-23 14:27:11 +0200

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -32,7 +32,7 @@ class About extends Component {
                      <span>louiscarlier123@gmail.com</span>
 					   </p>
                </div>
-               <br></br><br></br>
+               <br></br><br></br> 
                <div className="columns download">
                   <p>
                      <a href="../pdf/CV.pdf" target="_blank" className="button"><i className="fa fa-download"></i>Mon CV version pdf</a>
