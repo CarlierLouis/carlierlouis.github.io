e598ea612c3450358d6b439401f3f8daf1f99367
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2022-04-23 23:49:28 +0200

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -37,8 +37,7 @@ class About extends Component {
                <div className="columns download">
                   <p>
                      <a href="https://github.com/CarlierLouis/carlierlouis.github.io/blob/gh-pages/data/CV.pdf" target="_blank" className="button"><i className="fa fa-download"></i>Mon CV version pdf</a>
-                  </p>
-               </div>
+                  </p> 
             </div>
          </div>
       </div>
